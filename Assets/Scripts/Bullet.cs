269b9981ac1cf63269f6ae9a66e9b845951843b5
bradyamnitz <bradyamnitz@gmail.com>
2023-07-07 17:57:16 -0500

    added targets

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -21,6 +21,10 @@ public class Bullet : MonoBehaviour
 	private void OnTriggerEnter2D(Collider2D collision)
 	{
 		if (collision.gameObject.tag == "bullet") return;
+		if(collision.gameObject.tag == "target")
+		{
+			Destroy(collision.gameObject);
+		}
 		Destroy(gameObject);
 	}
 }
--- Assets/Scripts/Shotgun.cs
+++ Assets/Scripts/Shotgun.cs
@@ -7,12 +7,13 @@ public class Shotgun : Weapon //INHERITANCE
 
 	public Bullet bullet;
 	public Transform bulletSpawn;
-	private int pelletCount = 5;
+	[SerializeField] private int pelletCount = 5;
+	[SerializeField] private float pelletDeviation = 3f;
 
 	public override void Fire() //POLYMORPHISM
 	{
 		for (int i = 0; i < pelletCount; i++) {
-			Instantiate(bullet, bulletSpawn.position, Quaternion.Euler(new Vector3(0f, 0f, Random.Range(-3f, 3f))));
+			Instantiate(bullet, bulletSpawn.position, Quaternion.Euler(new Vector3(0f, 0f, Random.Range(-pelletDeviation, pelletDeviation))));
 		}
 		
 	}
